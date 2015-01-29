d199fbeadf4bdf5f0c36ae39aa4c63ca5364f48b
CBALDEMİR1995 <server.co.mertbaldemir@gmail.com>
2015-01-29 17:09:18 +0200

    OperatorIliskisi

---
+++ IliskiselOperatorl.java
@@ -0,0 +1,27 @@
+
+public class IliskiselOperatorl {
+
+	
+	public static void main(String[] args) {
+	
+		int sayi=100, sayi2=150, sayi3=250;
+		
+		 
+		boolean sonuc;
+		
+		
+		sonuc=sayi<sayi2;
+		System.out.println(sonuc);
+		
+		sonuc=sayi+sayi2>=sayi3;
+		System.out.println(sonuc);
+		
+		sonuc=sayi*sayi2<=sayi3;
+		System.out.println(sonuc);
+		
+		
+		
+
+	}
+
+}
