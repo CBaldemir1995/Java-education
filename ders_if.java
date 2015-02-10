4c51945f0584498c90989ed688631b64f67686e6
CBALDEMİR1995 <server.co.mertbaldemir@gmail.com>
2015-02-10 12:00:13 +0200

    İf kullanımı

---
+++ ders_if.java
@@ -0,0 +1,36 @@
+
+public class DersIf {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		boolean durum = true;
+		
+		if(durum)
+			
+		{
+			
+			System.out.println("Durum True");
+			
+		}
+		
+		
+		int puan = 99;
+		char harf='-';
+		
+		if(puan>=90)
+			
+		{
+			harf='A';
+			System.out.println("Başarı Harfiniz" + " " +harf );
+			
+			
+		}
+		
+		
+	}
+
+}
